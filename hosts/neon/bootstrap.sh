bc01bbd92a97bc658737d90051a85b3846a71b2a
psevdaisthisi <code@fmoreira.art>
2020-10-17 09:30:12 +0100

    bootstrap: Add more packages to the list
    `os-prober' and `tigervnc'

--- hosts/neon/bootstrap.sh
+++ hosts/neon/bootstrap.sh
@@ -171,9 +171,10 @@ _pkgs_sys=(atk avahi bash bluez coreutils dhcpcd efibootmgr exfat-utils
 _pkgs_tools=(archiso aria2 bash-completion bat bc bluez-utils cpupower croc curl
              entr exa fd ffmpeg firejail fwupd fzf gawk gnupg gocryptfs go-ipfs
              htop inotify-tools intel-gpu-tools intel-undervolt iotop libva-utils
-             lshw lsof man neovim nmap nnn openbsd-netcat openssh p7zip pbzip2
-             pigz powertop progress ripgrep samba svt-av1 svt-hevc svt-vp9 time
-             tmux tree turbostat unzip usbutils usleep which xdg-user-dirs)
+             lshw lsof man neovim nmap nnn openbsd-netcat openssh os-prober p7zip
+             pbzip2 pigz powertop progress ripgrep samba svt-av1 svt-hevc svt-vp9
+             tigervnc time tmux tree turbostat unzip usbutils usleep which
+             xdg-user-dirs)
 _pkgs_dev=(afl autoconf automake binaryen binutils bison clang cmake ctags
            diffutils docker docker-compose edk2-ovmf gcc gcc8 gcc9 gdb git go
            go-tools intel-media-sdk lldb m4 make man-pages ninja openssl-1.0 perf

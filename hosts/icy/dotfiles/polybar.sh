bb670ade1f49200a7eb91ee3061ff13b72c3f056
psevdaisthisi <code@fmoreira.art>
2020-10-26 20:04:27 +0000

    icy/polybar: Fix single monitor configuration

--- hosts/icy/dotfiles/polybar.sh
+++ hosts/icy/dotfiles/polybar.sh
@@ -2,5 +2,5 @@ if [ "$(xrandr | grep 'DisplayPort-0 connected')" ]; then
 	polybar internal &> "$HOME/.local/share/polybar/external.log" &
 	polybar external &> "$HOME/.local/share/polybar/external.log" &
 else
-	polybar main &> "$HOME/.local/share/polybar/main.log" &
+	polybar solo &> "$HOME/.local/share/polybar/main.log" &
 fi

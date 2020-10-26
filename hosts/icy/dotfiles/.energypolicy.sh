c013e57e6dd68e31f427a8b57752fb1be4b8df3a
psevdaisthisi <code@fmoreira.art>
2020-10-26 20:17:51 +0000

    Make some minor changes

--- hosts/icy/dotfiles/.energypolicy.sh
+++ hosts/icy/dotfiles/.energypolicy.sh
@@ -3,11 +3,6 @@ if [ "$1" = "default" ]; then
 	sudo cpupower frequency-set --max "4.00GHz" > /dev/null && \
 	echo "$1" >> "$XDG_CONFIG_HOME/.energypolicy"
 
-elif [ "$1" = "performance" ]; then
-	sudo cpupower frequency-set --governor performance > /dev/null && \
-	sudo cpupower frequency-set --max "4.00GHz" > /dev/null && \
-	echo "$1" >> "$XDG_CONFIG_HOME/.energypolicy"
-
 elif [ "$1" = "balanced" ]; then
 	sudo cpupower frequency-set --governor ondemand > /dev/null && \
 	sudo cpupower frequency-set --max "3.30GHz" > /dev/null && \
@@ -16,5 +11,10 @@ elif [ "$1" = "balanced" ]; then
 elif [ "$1" = "powersave" ]; then
 	sudo cpupower frequency-set --governor powersave > /dev/null && \
 	sudo cpupower frequency-set --max "2.30GHz" > /dev/null && \
+	echo "$1" >> "$XDG_CONFIG_HOME/.energypolicy"
+
+elif [ "$1" = "performance" ]; then
+	sudo cpupower frequency-set --governor performance > /dev/null && \
+	sudo cpupower frequency-set --max "4.00GHz" > /dev/null && \
 	echo "$1" >> "$XDG_CONFIG_HOME/.energypolicy"
 fi

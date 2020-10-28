a7bc206d8bec5a2e88b4190573516ef1516f05d4
psevdaisthisi <code@fmoreira.art>
2020-10-28 11:33:36 +0000

    icy/polybar: Fix names of log files

--- hosts/icy/dotfiles/polybar.sh
+++ hosts/icy/dotfiles/polybar.sh
@@ -1,6 +1,6 @@
 if [ "$(xrandr | grep 'DisplayPort-0 connected')" ]; then
-	polybar internal &> "$HOME/.local/share/polybar/external.log" &
+	polybar internal &> "$HOME/.local/share/polybar/internal.log" &
 	polybar external &> "$HOME/.local/share/polybar/external.log" &
 else
-	polybar solo &> "$HOME/.local/share/polybar/main.log" &
+	polybar solo &> "$HOME/.local/share/polybar/solo.log" &
 fi

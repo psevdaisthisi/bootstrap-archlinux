bcb34d345e45349a4589e1e609cd6b54551c791f
psevdaisthisi <code@fmoreira.art>
2020-10-31 10:23:11 +0000

    polybar: Add format options to `fsusage' module

--- users/psevdaisthisi/plugins/polybar/fsusage.sh
+++ users/psevdaisthisi/plugins/polybar/fsusage.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env sh
 
-partition=$1
+partition="$1"
+periodsec="$2"
+fmt="$3"
 
 while :
 do
 	info=$(df | grep "$partition")
-	echo \
-		$(echo "$info" | awk '{printf "scale=1; %s / 1024 / 1024\n", $3}' | bc | awk '{printf "%sGiB", $1}') \
-		$(echo "$info" | awk '{printf "(%s)", $5}')
-	# echo $(echo "$info" | awk '{printf "(%s)", $5}')
+	used_spc="$(echo "$info" | awk '{printf "scale=1; %s / 1024 / 1024\n", $3}' | bc | awk '{printf "%sGiB", $1}')"
+	used_perc="$(echo "$info" | awk '{printf "%s", $5}')"
+
+	[ "$fmt" = "full" ] && echo "$used_spc ($used_perc)"
+	[ "$fmt" = "space" ] && echo "$used_spc"
+	[ "$fmt" = "percentage" ] && echo "$used_perc"
+
 	sleep $2
 done

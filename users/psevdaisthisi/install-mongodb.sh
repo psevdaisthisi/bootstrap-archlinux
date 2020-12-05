81d78faa70bb026f1a731e76d928a33c588ed885
psevdaisthisi <code@fmoreira.art>
2020-12-05 17:12:34 +0000

    Fix MongoDB archive extraction

--- users/psevdaisthisi/install-mongodb.sh
+++ users/psevdaisthisi/install-mongodb.sh
@@ -5,8 +5,8 @@ curl --connect-timeout 13 --retry 5 --retry-delay 2 \
 	-L 'https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian10-4.4.2.tgz' \
 	-o /tmp/mongodb.tgz &&
 mkdir -p /tmp/mongodb /tmp/mongodb-tools &&
-tar fvxz mongodb.tgz -C /tmp/mongodb/ --wildcards  '*/bin/*' --strip-components=1 &&
-tar fvxz mongodb-tools.tgz -C /tmp/mongodb-tools/ --wildcards  '*/bin/*' --strip-components=1 &&
+tar fvxz /tmp/mongodb.tgz -C /tmp/mongodb/ --wildcards  '*/bin/*' --strip-components=1 &&
+tar fvxz /tmp/mongodb-tools.tgz -C /tmp/mongodb-tools/ --wildcards  '*/bin/*' --strip-components=1 &&
 mv /tmp/mongodb/bin/mongo* "$HOME/.local/bin/" &&
 mv /tmp/mongodb-tools/bin/mongo* "$HOME/.local/bin/" &&
 rm -rf /tmp/mongodb*
